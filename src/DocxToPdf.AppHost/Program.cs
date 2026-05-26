1e44e0f794c482ed645bb577cd8cba8097094030
iRumba <reloader2003@gmail.com>
2026-05-26 15:18:09 +0700

    bugfix: add 'dev' script name to AddNpmApp (fixes #19)
    AddNpmApp by default runs 'npm run start', but the client's
    package.json only has a 'dev' script. This causes the client
    to immediately exit with code 1 when launched via Aspire.
    
    Closes #19

--- src/DocxToPdf.AppHost/Program.cs
+++ src/DocxToPdf.AppHost/Program.cs
@@ -4,7 +4,7 @@ var apiService = builder.AddProject<Projects.DocxToPdf_Server>("api")
     .WithExternalHttpEndpoints()
     .WithEnvironment("ASPNETCORE_URLS", "http://+:8080");
 
-builder.AddNpmApp("client", "../DocxToPdf.Client")
+builder.AddNpmApp("client", "../DocxToPdf.Client", "dev")
     .WithReference(apiService)
     .WithEndpoint(port: 5173, scheme: "http", env: "PORT")
     .WithEnvironment("VITE_API_URL", apiService.GetEndpoint("http"))
